7dcb6549246b7b295d3fd48767b0726ba5b571ea
jaydip gabani <jaydipgabani.tcoso@gmail.com>
2023-11-02 17:21:09 +0530

    changes on latest code

--- app/Services/Api/ItemService.php
+++ app/Services/Api/ItemService.php
@@ -104,8 +104,19 @@ class ItemService
             $getProductByCatId['product'] = DB::table('items')->where('items.item_id', $request->item_id)->get();
 
             foreach ($getProductByCatId['product'] as $rating) {
+                $gete=
                 $rating->rating = DB::table('reviewandratings')
                     ->where('item_id', $rating->item_id)->avg('rating');
+                    $like_status = DB::table('favorites')
+                    ->where('user_id',$request->user_id)
+                    ->where('item_id', $rating->item_id)
+                    ->first();
+                    if($like_status){
+                        $rating->like_status=1;
+                    }else{
+                        $rating->like_status=0;
+                    }
+
             }
 
             if (count($getProductByCatId) > 0) {
